34025a3a984ac49403325c0cfa55e2b6fe4755af
marianafocassio <mariana.focassio@gmail.com>
2022-09-27 15:08:53 -0300

    status tarefa

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,15 +1,18 @@
-import styles from './Task.module.css'
+import styles from './Task.module.css';
 
-import checkIcon from '../assets/check.svg'
-import emptyCheckIcon from '../assets/empty-check.svg'
-import trashIcon from '../assets/trash-icon.svg'
+import checkIcon from '../assets/check.svg';
+import emptyCheckIcon from '../assets/empty-check.svg';
+import trashIcon from '../assets/trash-icon.svg';
 
 interface TaskProps {
+   id: string,
    title: string,
    isIncomplete: boolean,
+   //função que recebe id como parâmetro mas que começa vazia
 }
 
-export function Task({ title, isIncomplete = true }: TaskProps) {
+export function Task({ title, isIncomplete}: TaskProps) {
+   
    return (
       <div className={styles.task}>
          <button title='Mudar status da tarefa'>
@@ -17,10 +20,11 @@ export function Task({ title, isIncomplete = true }: TaskProps) {
             src={isIncomplete ? emptyCheckIcon : checkIcon}
             />
          </button>
+         {/* condition ? exprTrue : exprFalse */}
          <p className={isIncomplete ? styles.taskIsIncomplete : styles.taskIsComplete}>{title}</p>
          <button title='Deletar tarefa'>
             <img src={trashIcon} />
          </button>
       </div>
-   )
+   );
 }
